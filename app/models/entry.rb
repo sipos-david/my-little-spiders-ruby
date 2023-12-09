dbbae3939f2ad4aa2626f7e9f2ec978c6739fc26
Sipos Dávid <sipisipos@gmail.com>
2023-12-09 13:01:55 +0000

    Add validation to models

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,11 @@
 class Entry < ApplicationRecord
   belongs_to :roommate
+  after_initialize :init
+  validates :text, presence: true
+  validates :respect_gained, presence: true
+  validates :date, presence: true
+
+  def init
+    self.date ||= Time.now if self.new_record?
+  end
 end
--- app/models/roommate.rb
+++ app/models/roommate.rb
@@ -1,3 +1,11 @@
 class Roommate < ApplicationRecord
-    has_many :entries, dependent: :destroy
+  has_many :entries, dependent: :destroy
+  after_initialize :init
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :num_of_nightmares, presence: true, :numericality => { greater_than_or_equal_to: 0 }
+
+  def init
+    self.num_of_nightmares ||= 0 if self.new_record?
+  end
 end
